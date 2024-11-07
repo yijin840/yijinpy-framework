2695ad5b300d66a6b584cca94756087e3a1d921c
yijin840 <2900086967@qq.com>
2024-11-07 18:09:17 +0800

    modify simple_torch_test load path

--- src/simple_torch.py
+++ src/simple_torch.py
@@ -4,8 +4,8 @@ import sys
 
 def loadDataStore(ds_path):
     data_lines = []
-    with open(ds_path, "r") as f:
+    with open(ds_path, "r", encoding="utf-8") as f:
         data_lines.append(f.readline())
-    f.closed
+    f.close()
 
     print(data_lines)
