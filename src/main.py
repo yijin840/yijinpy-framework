5b33b042ba114d1cb847c4de9822f738c0b79d37
yijin840 <2900086967@qq.com>
2024-11-08 14:50:36 +0800

    add test

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@
 
 import os
 from t_model import TModel
-
+from torch_utils import TorchUtils
 import numpy_utils
 import simple_linear_model
-import torch_utils
 import simple_torch
+
 
 def run():
     """run"""
@@ -21,12 +21,14 @@ def test():
     print("test test")
     # numpy_arr()
     # testSimpleLinerModel()
-    torch_utils.printData()
+    TorchUtils.printData()
 
 
 def simple_torch_test():
     current_working_dir = os.getcwd()
-    simple_torch.loadDataStore(current_working_dir + "/yijinpy-framework/resources/WikiQACorpus/WikiQA-dev.txt")
+    simple_torch.loadDataStore(
+        current_working_dir + "/yijinpy-framework/resources/WikiQACorpus/WikiQA-dev.txt"
+    )
 
 
 if __name__ == "__main__":
--- src/torch_utils.py
+++ src/torch_utils.py
@@ -5,31 +5,33 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 
-# Download training data from open datasets.
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
+class TorchUtils:
+    def __init__(self):
+        # Download training data from open datasets.
+        self.training_data = datasets.FashionMNIST(
+            root="data",
+            train=True,
+            download=True,
+            transform=ToTensor(),
+        )
+        # Download test data from open datasets.
+        self.test_data = datasets.FashionMNIST(
+            root="data",
+            train=False,
+            download=True,
+            transform=ToTensor(),
+        )
+        self.batch_size = 64
+        # Create data loaders.
+        self.train_dataloader = DataLoader(
+            self.training_data, batch_size=self.batch_size
+        )
+        self.test_dataloader = DataLoader(self.test_data, batch_size=self.batch_size)
 
-# Download test data from open datasets.
-test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor(),
-)
-
-batch_size = 64
-# Create data loaders.
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
-
-def printData():
-    print("print data:")
-    for X, y in test_dataloader:
-        print(f"Shape of X [N, C, H, W]: {X.shape}")
-        print(f"Shape of y: {y.shape} {y.dtype}")
-        break
+    @staticmethod
+    def printData(self):
+        print("print data:")
+        for X, y in self.test_dataloader:
+            print(f"Shape of X [N, C, H, W]: {X.shape}")
+            print(f"Shape of y: {y.shape} {y.dtype}")
+            break
