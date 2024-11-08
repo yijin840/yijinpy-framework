cf253731d7d4c5156c4c8e03c8daab09e14a87c3
yijin840 <2900086967@qq.com>
2024-11-08 15:03:39 +0800

    add NeuralNetwork

--- src/torch_utils.py
+++ src/torch_utils.py
@@ -39,7 +39,7 @@ class TorchUtils:
 class NeuralNetwork(nn.Module):
 
     def __init__(self):
-        super()._init__()
+        super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(28 * 28, 512),
