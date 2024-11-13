f4882e2d2072ab800bf005055e43b87717f2a95a
yijin840 <2900086967@qq.com>
2024-11-13 14:58:31 +0800

    test

--- src/main.py
+++ src/main.py
@@ -4,10 +4,6 @@
 import sys
 import os
 
-abspath = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-print(abspath)
-sys.path.append(abspath)
-sys.path.append(abspath + "/src/test")
 import test.torch_test as torch_test
 import test.simple_linear_model as simple_linear_model
 import test.numpy_utils as numpy_utils
