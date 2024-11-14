1144b8bd513067b24a5e2f0645fc5bf2ccec5b67
yijin840 <2900086967@qq.com>
2024-11-14 18:32:14 +0800

    modify model

--- src/yijin_model.py
+++ src/yijin_model.py
@@ -4,49 +4,6 @@ from datasets import load_dataset
 import torch.optim as optim
 
 
-class YijinTranslateModel:
-
-    def __init__(self):
-        print("yijin translate model init.")
-        pass
-
-    ## 数据预处理
-    def pre_process_data(self):
-        pass
-
-    ## 构建词汇表
-    def build_vocabulary(self):
-        pass
-
-    ## 数据加载
-    def load_data_store(self):
-        pass
-
-    ## 训练
-    def train(self):
-        pass
-
-    ##保存数据
-    def save_data_store(self):
-        pass
-
-    ## 创建数据
-    def create_data_store(self):
-        pass
-
-    ## 创建模型
-    def create_model(self):
-        pass
-
-    ## 加载模型
-    def load_model(self):
-        pass
-
-    ## 导入模型
-    def save_model(self):
-        pass
-
-
 ## 对话模型 基于GPT-2 训练
 class YijinGptModel:
     """
@@ -58,18 +15,22 @@ class YijinGptModel:
     6. 交互
     """
 
-    def __init__(self, data_store=None):
+    def __init__(self, state_dict_path=None):
         print("init yijin gpt model.")
-        self.model = self.create_default_model()
-        self.data_store = (
-            self.load_data_store(data_store) or self.load_default_dataset()
-        )
-        self.tokenizer = self.get_default_tokenizer()
+        ## 初始化默认模型
+        self.model = GPT2LMHeadModel.from_pretrained("gpt2")
+        self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        self.tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+        ## 默认数据源
+        self.datasets = self.load_default_dataset()
+        if state_dict_path is None:
+            self.state_dict = self.model.state_dict()
+        else:
+            checkpoint = torch.load(state_dict_path)
+            # 加载模型权重
+            self.state_dict = self.model.load_state_dict(checkpoint, strict=False)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
-
-    def create_default_model(self):
-        return GPT2LMHeadModel.from_pretrained("gpt2")
 
     def tokenize_function(self, ds):
         tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
@@ -78,33 +39,28 @@ class YijinGptModel:
         )
 
     def load_default_dataset(self):
-        # dataset = load_dataset("roycehe/tieba")
-        # self.train_dataset = dataset["train"].map(self.tokenize_function, batched=True)
-        # self.validation_dataset = dataset["validation"].map(
-        #     self.tokenize_function, batched=True
-        # )
-        # self.test_dataset = dataset["test"].map(self.tokenize_function, batched=True)
+        dataset = load_dataset("roycehe/tieba")
+        self.train_dataset = dataset["train"].map(self.tokenize_function, batched=True)
+        self.validation_dataset = dataset["validation"].map(
+            self.tokenize_function, batched=True
+        )
+        self.test_dataset = dataset["test"].map(self.tokenize_function, batched=True)
         # print(dataset)
-        # return dataset
+        return dataset
         # # return load_dataset(
         # # "text",
         # # data_files="https://huggingface.co/datasets/lhoestq/test/resolve/main/some_text.txt",
         # # )
-        return load_dataset("daily_dialog", trust_remote_code=True)
-
-    def get_default_tokenizer(self):
-        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-        tokenizer.add_special_tokens({"pad_token": "[PAD]"})
-        return tokenizer
+        # return load_dataset("daily_dialog", trust_remote_code=True)
 
     def train(self, epochs=3, batch_size=4, learning_rate=5e-5, max_length=512):
         optimizer = optim.AdamW(self.model.parameters(), lr=learning_rate)
         for epoch in range(epochs):
-            print(f"Epoch {epoch + 1}/{epochs}")  # 打印当前的 epoch
-            epoch_loss = 0  # 用于记录当前 epoch 的总损失
+            print(f"Epoch {epoch + 1}/{epochs}")
+            epoch_loss = 0
 
             # 加载训练数据
-            inputs_data = self.data_store["train"]["dialog"]  # 保留原始数据
+            inputs_data = self.data_store["train"]["dialog"]
 
             for i in range(0, len(inputs_data), batch_size):
                 batch = [" ".join(dialog) for dialog in inputs_data[i : i + batch_size]]
@@ -130,7 +86,7 @@ class YijinGptModel:
                 # 检查是否有超出词汇表范围的 token
                 input_ids = tokenized_inputs["input_ids"]
                 if input_ids.max().item() >= self.model.config.vocab_size:
-                    print("Warning: Detected out-of-vocabulary token in input_ids")
+                    # print("Warning: Detected out-of-vocabulary token in input_ids")
                     continue  # 跳过这个 batch
 
                 optimizer.zero_grad()
@@ -141,7 +97,7 @@ class YijinGptModel:
                 loss = outputs.loss
                 loss.backward()
                 optimizer.step()
-                epoch_loss += loss.item()  # 累加当前 batch 的损失
+                epoch_loss += loss.item()
 
             # 打印每个 epoch 的平均损失
             print(f"Epoch {epoch + 1} Loss: {epoch_loss / len(inputs_data)}")
@@ -170,6 +126,3 @@ class YijinGptModel:
 
     def save_data_store(self, model_path):
         torch.save(self.model.state_dict(), model_path)
-
-    def load_data_store(self, model_path):
-        self.model.load_state_dict(torch.load(model_path, weights_only=True))
